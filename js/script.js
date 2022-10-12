7dee6160664f4fd10f70508ada92dfd19b83751c
Boldizsar Szabo <szboldisz@gmail.com>
2022-10-12 11:48:17 +0200

    Add hovering effect with one color

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ const containerDiv = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++) {
   const newDiv = containerDiv.appendChild(document.createElement("div"));
+  newDiv.classList.add("box");
 }
+
+const gridBox = document.querySelectorAll(".box");
+gridBox.forEach((item) => {
+  item.addEventListener("mouseover", function () {
+    item.style.backgroundColor = "rgb(50, 50, 50)";
+  });
+});

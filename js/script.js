f6e5c209a436942b6058938b572caa99234ce3f6
Boldizsar Szabo <szboldisz@gmail.com>
2022-10-12 18:56:37 +0200

    Add button for changing number of squares

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
 const containerDiv = document.querySelector(".container");
+const btnPixel = document.querySelector(".btn-pixel");
+const createDiv = document.createElement("div");
 
-for (let i = 0; i < 256; i++) {
-  const newDiv = containerDiv.appendChild(document.createElement("div"));
+let pixelNum = 20;
+for (let i = 0; i < pixelNum ** 2; i++) {
+  const newDiv = containerDiv.appendChild(createDiv.cloneNode(true));
   newDiv.classList.add("box");
 }
 
-const gridBox = document.querySelectorAll(".box");
-gridBox.forEach((item) => {
+document.querySelectorAll(".box").forEach((item) => {
   item.addEventListener("mouseover", function () {
     item.style.backgroundColor = "rgb(50, 50, 50)";
   });
+});
+
+btnPixel.addEventListener("click", function () {
+  const userChoice = prompt("Number of squares per side:", 20);
+  if (userChoice > 100) {
+    alert("Please choose a smaller number");
+  } else if (userChoice < 1) {
+    alert("Please choose a larger number");
+  } else {
+    pixelNum = userChoice;
+    containerDiv.style.gridTemplateRows = `repeat(${pixelNum}, 1fr)`;
+    containerDiv.style.gridTemplateColumns = `repeat(${pixelNum}, 1fr)`;
+
+    for (let i = 0; i < pixelNum ** 2; i++) {
+      newDiv = containerDiv.appendChild(createDiv.cloneNode(true));
+      newDiv.classList.add("box");
+    }
+
+    document.querySelectorAll(".box").forEach((item) => {
+      item.style.backgroundColor = "rgb(255, 255, 255)";
+      item.addEventListener("mouseover", function () {
+        item.style.backgroundColor = "rgb(50, 50, 50)";
+      });
+    });
+  }
 });
